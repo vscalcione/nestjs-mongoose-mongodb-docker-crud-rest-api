f2d1a04fad6ee6bc6c5cdc41fcce8216bedf944d
vscalcione <vincenzo99.scalcione@gmail.com>
2021-07-05 19:10:21 +0200

    Update some files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/controller/users.controller';
-import { UsersService } from './users/service/users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UsersModule } from './users/users.module';
 
+
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    UsersModule, 
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.ffayr.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+import { UserSchema } from './model/user.model';
+import { UsersController } from './controller/users.controller';
+import { UsersService } from './service/users.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
 export class UsersModule {}
